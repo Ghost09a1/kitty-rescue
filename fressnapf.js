53576b86955687ca6683ef6a67f83739597e6d4c
Ghost09a1 <ghost09a1@googlemail.com>
2023-08-11 13:50:14 +0200

    try4

--- fressnapf.js
+++ fressnapf.js
@@ -1,14 +1,14 @@
+// Function to perform the search on Fressnapf
 async function searchFressnapf(query) {
-    const searchUrl = `https://www.fressnapf.de/suche?search=${encodeURIComponent(
-        query
-    )}`;
+    const searchUrl = `https://www.fressnapf.de/suche?search=${encodeURIComponent(query)}`;
 
     try {
         const response = await fetch(searchUrl);
 
         if (response.ok) {
             const text = await response.text();
-            console.log(text);
+            // Handle the search results here, e.g., display them on your webpage
+            console.log("Search results:", text);
         } else {
             console.error("Failed to fetch the search results.");
         }
@@ -17,33 +17,15 @@ async function searchFressnapf(query) {
     }
 }
 
+// Get references to the search button and input field
 const searchButton = document.getElementById("searchBtn");
 const input = document.getElementById("search");
 
-function performSearch() {
+// Function to handle the search button click
+function handleSearchButtonClick() {
     const searchTerm = input.value;
     searchFressnapf(searchTerm);
 }
 
-searchButton.addEventListener("click", performSearch);
-
-const donationButton = document.getElementById("btn");
-const cat = document.getElementById("cat");
-
-function handleDonation() {
-    const amount = input.value;
-
-    if (amount == 0) {
-        alert("Thanks for nothing, the cats are all sad :(");
-        document.body.style.background = "red";
-    } else {
-        alert(`Thanks for the donation of $ ${amount}!`);
-    }
-}
-
-function catClick() {
-    document.body.style.background = "pink";
-}
-
-donationButton.addEventListener("click", handleDonation);
-cat.addEventListener("click", catClick);
+// Add event listener for the search button
+searchButton.addEventListener("click", handleSearchButtonClick);
