781cf7688aa961340fa5759bc643c16ce5e67d32
Ghost09a1 <ghost09a1@googlemail.com>
2023-08-11 15:17:29 +0200

    try 5

--- fressnapf.js
+++ fressnapf.js
@@ -1,13 +1,14 @@
-// Function to perform the search on Fressnapf
 async function searchFressnapf(query) {
-    const searchUrl = `https://www.fressnapf.de/suche?search=${encodeURIComponent(query)}`;
+    const searchUrl = `https://www.fressnapf.de/suche?search=${encodeURIComponent(
+        query
+    )}`;
 
     try {
         const response = await fetch(searchUrl);
 
         if (response.ok) {
             const text = await response.text();
-            // Handle the search results here, e.g., display them on your webpage
+
             console.log("Search results:", text);
         } else {
             console.error("Failed to fetch the search results.");
@@ -17,15 +18,13 @@ async function searchFressnapf(query) {
     }
 }
 
-// Get references to the search button and input field
 const searchButton = document.getElementById("searchBtn");
 const input = document.getElementById("search");
 
-// Function to handle the search button click
 function handleSearchButtonClick() {
     const searchTerm = input.value;
+    console.log({ searchButton, searchTerm, input });
     searchFressnapf(searchTerm);
 }
 
-// Add event listener for the search button
 searchButton.addEventListener("click", handleSearchButtonClick);
